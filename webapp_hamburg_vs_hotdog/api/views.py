883d29686c935516de01c57c565c0322cc6f5cd3
BlakeXYZ <37947050+BlakeXYZ@users.noreply.github.com>
2025-06-15 00:01:24 -0500

    check if clicktest db exists, if none, commit one [deploy]

--- webapp_hamburg_vs_hotdog/api/views.py
+++ webapp_hamburg_vs_hotdog/api/views.py
@@ -8,6 +8,10 @@ blueprint = Blueprint("api", __name__)
 @blueprint.route("/api/click/", methods=["POST"])
 def api_click():
     click_test = db.session.query(ClickTest).get(1)
+    if not click_test:
+        click_test = ClickTest(id=1, click_count=0)
+        db.session.add(click_test)
+        db.session.commit()
     if click_test:
         click_test.click_count += 1
         db.session.commit()
