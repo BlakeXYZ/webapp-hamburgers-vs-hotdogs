80c3398d04d874883f9a844eadc881115416c94f
BlakeXYZ <37947050+BlakeXYZ@users.noreply.github.com>
2025-07-01 22:40:38 -0500

    voting javascript logic - work in progress

--- webapp_hamburg_vs_hotdog/blueprints/voting/views.py
+++ webapp_hamburg_vs_hotdog/blueprints/voting/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, jsonify
+from flask import request
 
 from webapp_hamburg_vs_hotdog.database import db
 from webapp_hamburg_vs_hotdog.blueprints.voting.models import Contestant, Matchup, Vote
@@ -7,4 +8,25 @@ blueprint = Blueprint("voting", __name__)
 
 @blueprint.route("/on_click_vote/", methods=["POST"])
 def on_click_vote():
-    pass
+    data = request.get_json()
+    matchup_id = int(data['matchup_id'])
+    contestant_id = int(data['contestant_id'])
+
+    #TODO: allow for session user to switch vote, but only vote once per session
+    db.session.add(Vote(matchup=matchup_id,
+                        contestant=contestant_id,
+                        session_id=3,
+                        country_code="DE" ))
+    db.session.commit()
+
+    matchup = Matchup.query.get(matchup_id)
+    votes_a = sum(1 for v in matchup.votes if v.contestant_id == matchup.contestant_a_id)
+    votes_b = sum(1 for v in matchup.votes if v.contestant_id == matchup.contestant_b_id)
+    return jsonify(
+        matchup_id=matchup_id,
+        total_votes=len(matchup.votes),
+        votes_a=votes_a,
+        votes_b=votes_b,
+        contestant_a_name=matchup.contestant_a.contestant_name,
+        contestant_b_name=matchup.contestant_b.contestant_name
+    )
