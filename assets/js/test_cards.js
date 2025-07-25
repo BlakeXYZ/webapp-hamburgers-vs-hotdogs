1794224f16abf1afdd53e9f28cef8881e11f0020
BlakeXYZ <37947050+BlakeXYZ@users.noreply.github.com>
2025-07-24 23:51:01 -0500

    test cards css [deploy]

--- assets/js/test_cards.js
+++ assets/js/test_cards.js
@@ -6,20 +6,18 @@ import Swiper from 'swiper/bundle';
 var swiper = new Swiper(".mySwiper", {
     effect: "cards",
     grabCursor: true,
-
+    speed: 500, // Slide transition speed in ms
     rewind: true,
     navigation: {
         nextEl: ".swiper-button-next",
         prevEl: ".swiper-button-prev",
     },
     cardsEffect: {
-        perSlideOffset: 85, // Increase for more left/right offset (default is 8)
+        perSlideOffset: 125, // Increase for more left/right offset (default is 8)
         perSlideRotate: 0,
         rotate: true, // You can also adjust rotation if desired
+        slideShadows: false, // Disable shadows for a cleaner look
     },
-
-
-
 });
 
 
