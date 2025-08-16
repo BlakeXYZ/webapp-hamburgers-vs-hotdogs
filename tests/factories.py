7345719f896c03e84ba09d5e30129071b57cca7f
BlakeXYZ <37947050+BlakeXYZ@users.noreply.github.com>
2025-08-16 16:45:07 -0500

    rmv user + login tests [deploy]

--- tests/factories.py
+++ tests/factories.py
@@ -19,19 +19,6 @@ class BaseFactory(SQLAlchemyModelFactory):
         sqlalchemy_session = db.session
 
 
-class UserFactory(BaseFactory):
-    """User factory."""
-
-    username = Sequence(lambda n: f"user{n}")
-    email = Sequence(lambda n: f"user{n}@example.com")
-    active = True
-
-    class Meta:
-        """Factory configuration."""
-
-        model = User
-
-    
 class ContestantFactory(BaseFactory):
     """Contestant factory."""
 
