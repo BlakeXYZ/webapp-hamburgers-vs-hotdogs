ec7e0196ae3d5dfa98517ba27fcecbcc8cfa3d75
BlakeXYZ <37947050+BlakeXYZ@users.noreply.github.com>
2025-08-28 01:12:24 -0500

    gallery stats wip [deploy]

---
+++ assets/js/gallery_click.js
@@ -0,0 +1,16 @@
+
+
+
+document.querySelectorAll('.gallery-contestant').forEach(btn => {
+  btn.addEventListener('click', function() {
+    const contestantId = this.getAttribute('data-contestant-id');
+    const extraInfo = this.querySelector('.contestant-extra-info'); // Example of another data attribute
+    if (this.classList.contains('active')) {
+        extraInfo.style.display = 'block';     
+        console.log('Clicked contestant is Active:', contestantId);
+        return;
+    }
+    extraInfo.style.display = 'none';
+    console.log('Clicked contestant is Inactive:', contestantId);
+  });
+});
