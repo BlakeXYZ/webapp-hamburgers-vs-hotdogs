78128b791f5ec8bc125c68a62a1cb5d9c493bad0
BlakeXYZ <37947050+BlakeXYZ@users.noreply.github.com>
2025-09-22 22:48:27 -0500

    gallery expand chevron icon helper [deploy]

--- assets/js/gallery_click.js
+++ assets/js/gallery_click.js
@@ -5,12 +5,17 @@ document.querySelectorAll('.gallery-contestant').forEach(btn => {
   btn.addEventListener('click', function() {
     const contestantId = this.getAttribute('data-contestant-id');
     const extraInfo = this.querySelector('.contestant-stats'); // Example of another data attribute
+    const chevronIcon = this.querySelector('.gallery-chevron-icon i');
     if (this.classList.contains('active')) {
-        extraInfo.style.display = 'block';     
-        console.log('Clicked contestant is Active:', contestantId);
+        extraInfo.style.display = 'block';
+        chevronIcon.classList.remove('fa-chevron-down');
+        chevronIcon.classList.add('fa-chevron-up');
+        // console.log('Clicked contestant is Active:', contestantId);
         return;
     }
     extraInfo.style.display = 'none';
-    console.log('Clicked contestant is Inactive:', contestantId);
+    chevronIcon.classList.remove('fa-chevron-up');
+    chevronIcon.classList.add('fa-chevron-down');
+    // console.log('Clicked contestant is Inactive:', contestantId);
   });
 });
